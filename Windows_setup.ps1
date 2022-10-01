6c437beca59b9b93589a8ceee715f56fbb9d68ff
Dhiraj Arya <105063606+itsdhirajarya@users.noreply.github.com>
2022-10-01 22:01:56 +0530

    Update Windows_setup.ps1

--- Windows_setup.ps1
+++ Windows_setup.ps1
@@ -33,7 +33,7 @@ $ProgressPreference = 'SilentlyContinue'
 $jdk18 = Get-WmiObject -Class Win32_Product -filter "Vendor='Oracle Corporation'" |where Caption -clike "Java(TM) SE Development Kit 18*"
 if (!($jdk18)){
     echo "`t`tDownnloading Java JDK-18 ....(size-195MB)"
-    #wget "https://download.oracle.com/java/18/latest/jdk-18_windows-x64_bin.exe" -O jdk-18.exe    
+    wget "https://download.oracle.com/java/18/latest/jdk-18_windows-x64_bin.exe" -O jdk-18.exe    
     echo "`n`t`tJDK-18 Downloaded, lets start the Installation process"
     start -wait jdk-18.exe
     rm jdk-18.exe
@@ -46,7 +46,7 @@ if (!($jdk18)){
 $jre8 = Get-WmiObject -Class Win32_Product -filter "Vendor='Oracle Corporation'" |where Caption -clike "Java 8 Update *"
 if (!($jre8)){
     echo "`n`t`tDownloading Java JRE ....(size-95MB)"
-    #wget "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246474_2dee051a5d0647d5be72a7c0abff270e" -O jre-8.exe
+    wget "https://javadl.oracle.com/webapps/download/AutoDL?BundleId=246474_2dee051a5d0647d5be72a7c0abff270e" -O jre-8.exe
     echo "`n`t`tJRE-8 Downloaded, lets start the Installation process"
     start -wait jre-8.exe
     rm jre-8.exe
@@ -60,12 +60,12 @@ if (Test-Path Burp-Suite-Pro.jar){
     echo "Burp Suite Professional JAR file is available.`nChecking its Integrity ...."
     if (((Get-Item Burp-Suite-Pro.jar).length/1MB) -lt 500 ){
         echo "`n`t`tFiles Seems to be corrupted `n`t`tDownloading Latest Burp Suite Professional ...."
-        #wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O "Burp-Suite-Pro.jar"
+        wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O "Burp-Suite-Pro.jar"
         echo "`nBurp Suite Professional is Downloaded.`n"
     }else {echo "File Looks fine. Lets proceed for Execution"}
 }else {
     echo "`n`t`tDownloading Latest Burp Suite Professional ....(size-520MB)"
-    #wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O Burp-Suite-Pro.jar
+    wget "https://portswigger-cdn.net/burp/releases/download?product=pro&version=&type=jar" -O Burp-Suite-Pro.jar
     echo "`nBurp Suite Professional is Downloaded.`n"
 }
 
